f1d18f218da3e56d4dc6a0881b47e43f0debc6c3
izumi matsuo <gurux2daddy@gmail.com>
2017-07-20 17:37:03 +0900

    add windows branch

--- install.sh
+++ install.sh
@@ -4,5 +4,10 @@ for f in .??*
 do
     [ $f = ".git" ] && continue
 
+    if [ `uname` = "Windows_NT" ]; then
+        cp ~/dotfiles/$f ~/$f
+        continue
+    fi
+
     ln -sfv ~/dotfiles/$f ~/$f
 done
